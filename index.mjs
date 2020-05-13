cae745e00d24df6fbce5790b877ba74c20526700
Stian Haklev <stian@minervaproject.com>
2020-05-13 10:45:10 +0200

    WIP

--- index.mjs
+++ index.mjs
@@ -1,8 +1,9 @@
 import train from "./scheduleTrainer.mjs";
 import { exec } from "child_process";
 import fs from "fs";
-const homedir = os.homedir();
+import os from "os";
 
+const homedir = os.homedir();
 const path = `${homedir}/.quicktrainer`;
 
 let input = {
@@ -15,16 +16,10 @@ let input = {
 const questions = [];
 
 const ask = question =>
-  console.log(
-    question,
-
-    `osascript -e 'tell application (path to frontmost application as text) to display dialog "${question}"? buttons {"No","Yes"} with icon stop'`
-  ) ||
   new Promise(resolve =>
     exec(
       `osascript -e 'tell application (path to frontmost application as text) to display dialog "${question}?" buttons {"No","Yes"} with icon stop'`,
       (error, stdout, stderr) => {
-        console.log(stdout, stderr);
         resolve(stdout === "button returned:Yes\n");
       }
     )
@@ -35,7 +30,6 @@ const tell = answer =>
     exec(
       `osascript -e 'tell application (path to frontmost application as text) to display dialog "Answer: ${answer}" buttons {"OK"} with icon stop'`,
       (error, stdout, stderr) => {
-        console.log(error, stdout, stderr);
         resolve();
       }
     )
@@ -46,14 +40,21 @@ const main = async () => {
     .filter(x => x[0] > 0 && x[0] < Date.now())
     .forEach(async f => {
       f[0] = 0;
-      ask(f[1]).then(async x => {
-        f[3].qualityAssessment = x ? 5 : 1;
+      ask(f[1]).then(async didKnow => {
+        f[3].qualityAssessment = didKnow ? 5 : 1;
+
+        if (didKnow) {
+          f[3].currentRepetition += 1;
+        } else {
+          f[3].currentRepetition = 0;
+        }
         const res = train(f[3]);
         console.log(res);
-        f[3].currentRepetition += 1;
         f[3].lastEasinessFactor = res.easiness;
         f[0] = Date.now() + (res.interval || 1) * 60 * 1000;
-        if (!x) {
+        const dateStr = `${new Date().getHours()}:${new Date().getMinutes()}`;
+        console.log(`${dateStr}: Rescheduling for ${res.interval}`);
+        if (!didKnow) {
           await tell(f[2]);
         }
       });
@@ -61,11 +62,11 @@ const main = async () => {
   if (fs.existsSync(path)) {
     const f = fs.readFileSync("/users/stian/.quicktrainer", "utf-8");
     const [q, a] = f.trim().split(",");
-    questions.push([Date.now() + 60 * 100 * 1, q, a, { ...input }]);
+    questions.push([Date.now() + 60 * 1000 * 1, q, a, { ...input }]);
     console.log(questions);
     fs.unlinkSync(path);
   }
-  setTimeout(main, 1000);
+  setTimeout(main, 10000);
 };
 
 main();
